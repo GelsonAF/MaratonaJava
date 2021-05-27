9c274395355c7a4695a6fb76f77cd3bf741449ba
gelso <gelso@DESKTOP-LO80210>
2021-05-27 18:50:23 -0300

    ajustado para adicionar o texto ao invés de sobrescrever

--- MaratonaJava/src/EscreverTxt/EscreverTxt.java
+++ MaratonaJava/src/EscreverTxt/EscreverTxt.java
@@ -4,18 +4,19 @@ package escrevertxt;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 
 public class EscreverTxt {
 
     public static void main(String[] args) {
-        Path caminho = Paths.get("C:/Users/Gelson/Desktop/teste.txt");
-        String texto = "teste a ser gravado no arquivo \ndois";
+        Path caminho = Paths.get("C:/Users/gelso/Desktop/teste.txt");
+        String texto = "teste a ser gravado no arquivo \ndois\n";
         byte[] textoEmByte = texto.getBytes();
         try{
-        Files.write(caminho, textoEmByte);
-    }catch(Exception erro) {
-        
+        Files.write(caminho, textoEmByte, StandardOpenOption.APPEND);//APPEND para adicionar e não sobrescrever
+        }catch(Exception erro) {
+            System.out.println("não foi possivel escrever no arquivo");
     }
     
 }
